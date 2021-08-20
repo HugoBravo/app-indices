b8ffec5d0b3e00f06dc3b6e1c7872932d6502c92
Hugo Bravo <hbr4v0@gmail.com>
2021-08-19 22:11:54 -0400

    add unit tests over settingsService

--- src/app/pages/tabs/home/home.page.unit.spec.ts
+++ src/app/pages/tabs/home/home.page.unit.spec.ts
@@ -22,6 +22,4 @@ describe('HomePage Unit', () => {
 
   });
 
-
-
 });
--- src/app/services/settings.service.spec.ts
+++ src/app/services/settings.service.spec.ts
@@ -4,6 +4,8 @@ import { Drivers, Storage } from '@ionic/storage';
 
 import { SettingsService } from './settings.service';
 
+import defaultConfig from 'src/app/data/defaultConfig';
+
 describe('SettingsService', () => {
   let service: SettingsService;
 
@@ -22,4 +24,38 @@ describe('SettingsService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+
+  it('should call to storage when restoreIndiceConfigDefault() is called', () => {
+
+    const spy = spyOn( service, 'restoreIndiceConfigDefault').and.callFake(() => new Promise(null) );
+
+    service.restoreIndiceConfigDefault();
+
+    expect( spy ).toHaveBeenCalled();
+
+  });
+
+  it('should return false when indices key not exist in Storage', async() => {
+
+    const spy = spyOn( service, 'get').and.callFake(() => new Promise( (resolve, reject ) => 
+    {resolve( null ) }));
+
+    const has = await service.hasIndiceConfigData();
+    
+    expect( has ).toBeFalse();
+
+  });
+
+  it('should return true when indices key exist in Storage', async() => {
+
+    const spy = spyOn( service, 'get').and.callFake(() => new Promise( (resolve, reject ) => 
+    {resolve( defaultConfig ) }));
+
+    const has = await service.hasIndiceConfigData();
+    
+    expect( has ).toBeTrue();
+
+  });
+
 });
